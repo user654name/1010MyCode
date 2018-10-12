f53a5c3b783b69176737ba77fbbbbec3b7468fbd
user654name <ztqztq@qq.com>
2018-10-12 09:04:43 +0800

    2018年10月12日 09:04:34 早上学习

--- src/com/core/z_demos/JiSuanDemo.java
+++ src/com/core/z_demos/JiSuanDemo.java
@@ -1,6 +1,18 @@
 package com.core.z_demos;
 
+import java.util.Date;
+
 public class JiSuanDemo {
 
+    public static void main(String[] args) {
 
+
+        Date date = new Date();
+        date.setMinutes(29);
+        date.setSeconds(30);
+
+        String string = date.toString();
+        System.out.println("string = " + string);
+//        long i =  / 5;
+    }
 }
---
+++ src/com/core/z_demos/time/TimeDemo.java
@@ -0,0 +1,26 @@
+package com.core.z_demos.time;
+
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+import java.io.FileOutputStream;
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class TimeDemo {
+
+    public static void main(String[] args) {
+//        int timeRun = 29 * 60 + 35;
+//        int timeOne = timeRun / 5;
+//        int min = timeOne / 60;
+//        int sec = timeOne - min * 60;
+//        System.out.println(min + "分钟" + sec + "秒");
+
+//        Time time = new Time(0, 29, 35);
+
+
+
+
+    }
+
+}

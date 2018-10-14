c462941fab0b10943dcf046fd54814018a3f31f8
user654name <ztqztq@qq.com>
2018-10-14 23:05:49 +0800

    2018年10月14日 23:05:38 StringBuffer

---
+++ src/com/core/s_string/StringBufferDemo.java
@@ -0,0 +1,21 @@
+package com.core.s_string;
+
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+public class StringBufferDemo {
+
+
+    static StringBuffer stringBuffer = new StringBuffer();
+    static StringBuilder stringBuilder = new StringBuilder("nihao");
+
+    public static void main(String[] args) {
+
+
+        stringBuilder.append("!");
+//        stringBuilder.
+
+    }
+
+
+}
+
--- src/com/core/t_thread/MyThreadDemo.java
+++ src/com/core/t_thread/MyThreadDemo.java
@@ -2,4 +2,32 @@ package com.core.t_thread;
 
 public class MyThreadDemo {
 
+    public static void main(String[] args) {
+        MyThread1 thread1 = new MyThread1();
+        MyThread2 thread2 = new MyThread2();
+        thread1.start();
+        thread1.setPriority(3);
+
+        int priority = thread1.getPriority();
+        System.out.println("priority = " + priority);
+        thread2.run();
+
+
+    }
+
+}
+
+class MyThread1 extends Thread{
+    @Override
+    public void run() {
+
+        System.out.println("线程1 跑起来");
+    }
+}
+class MyThread2 implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println("线程2 跑起来");
+    }
 }

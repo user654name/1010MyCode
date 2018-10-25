e54b5bdb17b36ba545d2ac37e6711841921e4837
user654name <ztqztq@qq.com>
2018-10-25 09:34:29 +0800

    2018年10月25日 09:34:14 选择排序 手写成功！

--- src/com/core/s_sort/mypractice/WriteSelect.java
+++ src/com/core/s_sort/mypractice/WriteSelect.java
@@ -23,11 +23,27 @@ public class WriteSelect {
         arr[b] = arr[a];
         arr[a] = temp;
     }
-
+ /*
+    1 首先在未排序序列中找到最小（大）元素，存放到排序序列的起始位置
+    2 再从剩余未排序元素中继续寻找最小（大）元素，然后放到已排序序列的末尾。
+    3 重复第二步，直到所有元素均排序完毕。*/
 
     //在这里写排序代码
     public void sort(Integer[] nums) {
 
+        int length = nums.length;
+
+        for (int i = 0; i < length - 1; i++) {
+
+            int min = i;
+            //得未排序序列里到最小的数字 的位置
+            for (int j = i; j < length; j++) {
+                if (nums[j] < nums[min]) {
+                    min = j;
+                }
+            }
+            swap(nums, i, min);
+        }
 
     }
 

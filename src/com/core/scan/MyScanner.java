7450c6b37b590bd636804806aca64ce6dc1bdbe1
user654name <ztqztq@qq.com>
2019-01-11 16:24:30 +0800

    2019年1月11日 16:24:01 复习了控制台输入.... 不复习就全忘了

---
+++ src/com/core/scan/MyScanner.java
@@ -0,0 +1,33 @@
+package com.core.scan;
+
+import java.util.Scanner;
+
+/**
+ * @author Tianqi.Zhang
+ * @date 2019/1/11
+ * @time 16:19
+ * @package com.core.scan
+ * @project 1008MyDemo
+ * @description
+ */
+public class MyScanner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("请输入数字");
+            String next = scanner.next();
+
+            int num = 0;
+            try {
+                num = Integer.parseInt(next);
+            } catch (NumberFormatException e) {
+                System.out.println("转换异常");
+            }
+            int rst = num * 10;
+            System.out.println("rst = " + rst);
+
+        }
+
+    }
+}

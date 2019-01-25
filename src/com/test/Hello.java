3da976340a224196a1b3bb57b15d01c6d1eec801
user654name <ztqztq@qq.com>
2019-01-25 09:32:17 +0800

    2019年1月25日 09:32:07

--- src/com/test/Hello.java
+++ src/com/test/Hello.java
@@ -10,6 +10,6 @@ package com.test;
  */
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello 1234");
+        System.out.println("hello 12345");
     }
 }

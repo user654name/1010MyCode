96a69f9ee11913ef00788fea2de799c59cbebd33
user654name <ztqztq@qq.com>
2019-01-31 15:45:10 +0800

    I will go home by HighTrain Oh Yeah ！

--- src/com/test/Hello.java
+++ src/com/test/Hello.java
@@ -10,6 +10,6 @@ package com.test;
  */
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello 12345622317");
+        System.out.println("hello 12345622331217");
     }
 }

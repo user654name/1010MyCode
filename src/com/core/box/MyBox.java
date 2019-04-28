da8255519ab39e57989d871b15dc50dd92a98b28
user654name <ztqztq@qq.com>
2019-04-28 15:11:20 +0800

    5 1 soon

---
+++ src/com/core/box/MyBox.java
@@ -0,0 +1,17 @@
+package com.core.box;
+
+import javax.swing.*;
+
+/**
+ * @author Tianqi.Zhang
+ * @date 2019/4/11
+ * @time 15:12
+ * @package com.core.box
+ * @project 1008MyDemo
+ * @description
+ */
+public class MyBox {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "标题【出错啦】", "年龄请输入数字", JOptionPane.ERROR_MESSAGE);
+    }
+}

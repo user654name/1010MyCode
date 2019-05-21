8eec9f6474dfb825319f001ec8fbb622964c50de
user654name <ztqztq@qq.com>
2019-05-21 15:02:47 +0800

    java8 Date 语言新特性学习

---
+++ src/com/java8/date/Java8DateDemo.java
@@ -0,0 +1,13 @@
+package com.java8.date;
+
+/**
+ * @author Tianqi.Zhang
+ * @date 2019/5/21
+ * @time 15:01
+ * @package com.java8.date
+ * @project 1008MyDemo
+ * @description
+ */
+public class Java8DateDemo {
+
+}
--- src/flowable/service/TaskTest.java
+++ src/flowable/service/TaskTest.java
@@ -1,12 +1,20 @@
 package flowable.service;
 
+import flowable.service.util.Conn;
 import org.flowable.common.engine.impl.interceptor.CommandExecutor;
 import org.flowable.engine.ProcessEngine;
 import org.flowable.engine.ProcessEngineConfiguration;
 import org.flowable.engine.RuntimeService;
+import org.flowable.engine.TaskService;
 import org.flowable.engine.impl.RuntimeServiceImpl;
 import org.flowable.engine.impl.cfg.StandaloneProcessEngineConfiguration;
+import org.flowable.engine.runtime.ProcessInstance;
+import org.flowable.task.api.Task;
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Tianqi.Zhang
@@ -17,6 +25,53 @@ import org.junit.Test;
  * @description
  */
 public class TaskTest {
+
+    @Test
+    public void start() {
+        ProcessEngine engine = Conn.getEngine();
+        RuntimeService runtimeService = engine.getRuntimeService();
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "good");
+        map.put("time", "20190517");
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("roll-demo", map);
+        String id = processInstance.getId();
+        System.out.println("processInstanceId = " + id);
+        // processInstanceId = 5001
+    }
+
+    /*
+    taskId = 5008
+{name=good, time=20190517}
+
+taskId = 7504
+{name=bad, time=20190517, info=edit name from good to bad}
+     */
+    @Test
+    public void showTask() {
+        ProcessEngine engine = Conn.getEngine();
+        TaskService taskService = engine.getTaskService();
+        List<Task> list = taskService.createTaskQuery().includeProcessVariables().taskAssignee("zhang").processVariableValueEquals("time", "20190517").list();
+        for (Task task : list) {
+            String id = task.getId();
+            System.out.println("taskId = " + id);
+            Map<String, Object> variables = taskService.getVariables(task.getId());
+            System.out.println(variables);
+            Map<String, Object> processVariables = task.getProcessVariables();
+            System.out.println("processVariables = " + processVariables);
+        }
+
+    }
+
+    @Test
+    public void complete() {
+        ProcessEngine engine = Conn.getEngine();
+        TaskService taskService = engine.getTaskService();
+        Map<String, Object> map = new HashMap<>();
+        map.put("info", "edit name from good to bad");
+        map.put("name", "bad");
+        taskService.complete("5008", map);
+    }
+
 
     @Test
     public void back() {
